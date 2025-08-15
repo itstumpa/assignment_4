45cec43b594962c936b09da2351c34a6a4c47672
tumpa <tumpadas3494@gmail.com>
2025-08-15 17:05:41 +0600

    first

---
+++ qs_2.js
@@ -0,0 +1,20 @@
+
+
+function  onlyCharacter(str) {
+      if(typeof str !== "string"){
+            return "Invalid";
+      }
+      return str.replace(/\s+/g, "") .toUpperCase();
+}
+
+let string = [
+      "  h e llo wor   ld",
+      "Cy   bar- At  tac k  ",
+      " ha ck m e 1 @ru.c  n  ",
+      "Serv er : : Do wn",
+      ["hack", "me"],
+      true
+];      
+string.forEach(item =>{
+      console.log(onlyCharacter(item));
+});
